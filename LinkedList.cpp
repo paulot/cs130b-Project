ce3e8878e062aa7b0b562d6e0fbb7ff11219e87d
paulot <paulo.tanaka3@gmail.com>
2013-05-24 15:34:47 -0700

    Changed from Tuple to Node

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 template <class T>
-class Tuple {
+class Node {
     public:
-        Tuple(T& i):item(i) { 
+        Node(T& i):item(i) { 
             next = NULL;
         };
         T   &item;
-        Tuple<T>   *next;
+        Node<T>   *next;
         void print() {
             item.print();
         }
@@ -21,21 +21,21 @@ class LinkedList {
             size = 0;
         }
         ~LinkedList();
-        Tuple<T> &getRoot();
+        Node<T> &getRoot();
         void insert(T& item, int (*compar) (T& p1, T& p2));
         // Delete? what's that
         void print();
 
     private:
         int size;
-        Tuple<T>  *root;
+        Node<T>  *root;
 };
 
 template <class T>
 LinkedList<T>::~LinkedList() {
     if (root == NULL) return;
-    Tuple<T>* first = root;
-    Tuple<T>* second = root->next;
+    Node<T>* first = root;
+    Node<T>* second = root->next;
     for (first, second;
          second != NULL;
          first = second, second = second->next)
@@ -45,7 +45,7 @@ LinkedList<T>::~LinkedList() {
 template <class T>
 void LinkedList<T>::print() {
     if (root == NULL) return;
-    for (Tuple<T> *i = root; i != NULL; i = i->next)
+    for (Node<T> *i = root; i != NULL; i = i->next)
         i->print();
 }
      
@@ -61,19 +61,19 @@ void LinkedList<T>::print() {
 template <class T>
 void LinkedList<T>::insert(T& item, int (*compar) (T& p1, T& p2)) {
     if (root == NULL) {
-        root = new Tuple<T>(item);
+        root = new Node<T>(item);
         size++;
     } else {
-        Tuple<T> *first = root;
-        Tuple<T> *second = root->next;
+        Node<T> *first = root;
+        Node<T> *second = root->next;
         if (compar(item, first->item) < 0) { // item is before
-            root = new Tuple<T>(item);
+            root = new Node<T>(item);
             root->next = first;
             size++;
         } else {                             // item is after
             if (second == NULL) {
                     // NULL set?
-                    first->next = new Tuple<T>(item);
+                    first->next = new Node<T>(item);
                     size++;
             } else {                         // item is after
                 for (first, second;
@@ -82,11 +82,11 @@ void LinkedList<T>::insert(T& item, int (*compar) (T& p1, T& p2)) {
 
                 if (compar(item, first->item) > 0 and compar(item, second->item) < 0) {
                     // In between them
-                    first->next = new Tuple<T>(item);
+                    first->next = new Node<T>(item);
                     first->next->next = second;
                     size++;
                 } else {
-                    second->next = new Tuple<T>(item);
+                    second->next = new Node<T>(item);
                     size++;
                 }
             }
@@ -95,7 +95,7 @@ void LinkedList<T>::insert(T& item, int (*compar) (T& p1, T& p2)) {
 }
 
 template <class T>
-Tuple<T>& LinkedList<T>::getRoot() {
+Node<T>& LinkedList<T>::getRoot() {
     return root;
 }
 
